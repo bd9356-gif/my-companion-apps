512a17f363763d13da56fe2f1ee09a5bc59bfbc6
bd9356-gif <bd9356@gmail.com>
2026-07-19 16:24:12 -0400

    Add support page, fix MyGolf link

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'recipe.mycompanionapps.com' }],
+        destination: 'https://mycompanionapps.com/myrecipe',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
